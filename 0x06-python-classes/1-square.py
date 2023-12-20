5af44cd78b0182ac3b9a44f451f630f7a56a375e
cjeokoech <cjeokoech@gmail.com>
2023-12-20 02:22:44 -0800

    update

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" define a clss square"""
+
+
+class Square:
+    """ a class that define square
+
+    Attributes:
+    size: size of the square
+    """
+
+    def __init__(self, size):
+        """
+        Create a new instance of square
+
+        Args:
+        size: size of square
+        """
+
+        self.__size = size
