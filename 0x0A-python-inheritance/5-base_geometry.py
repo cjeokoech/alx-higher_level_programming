527a1f01ea59d78d09bc05447944e7384d83c0be
cjeokoech <cjeokoech@gmail.com>
2024-01-08 09:31:22 -0800

    update

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module 5-base_geometry.
+Write an empty class BaseGeometry"""
+
+
+class BaseGeometry:
+    """Empy class"""
+
+    pass
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module 6-base_geometry.
+Write a class BaseGeometry (based on 5-base_geometry.py)."""
+
+
+
+class BaseGeometry:
+    """Class with public method instance"""
+
+    def area(self):
+        """Raise exception """
+        raise Exception("area() is not implemented")
