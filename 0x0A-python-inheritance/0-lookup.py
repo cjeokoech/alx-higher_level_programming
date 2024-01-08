1e257f995981b07a87be60b98985a45072eec072
cjeokoech <cjeokoech@gmail.com>
2024-01-08 00:28:14 -0800

    update

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Return list of available attributes"""
+
+def lookup(obj):
+    """Return list
+
+    Args:
+    obj: object containing attributes
+
+    Returns:
+    list: of attributes
+    """
+    return dir(obj)
