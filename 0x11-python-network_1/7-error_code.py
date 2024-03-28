79b1b3f93e7c9e4ab5af2b9adc291de7d6af645f
cjeokoech <cjeokoech@gmail.com>
2024-03-28 11:18:16 -0700

    úpdate

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -6,8 +6,9 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    request = requests.get(url)
-    if rquest.status_code >= 400:
-        print("Error code: {}".format(request.status_code))
+
+    r = requests.get(url)
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
     else:
-        print(request.texti)
+        print(r.text)
--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -7,12 +7,13 @@ import sys
 if __name__ == "__main__":
     letter = "" if len(sys.argv) == 1 else sys.argv[1]
     payload = {"q": letter}
-    request = requests.post("http://0.0.0.0:5000/search_user", data=payload)
+
+    r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
     try:
-        r = response.json()
-        if r == {}:
+        response = r.json()
+        if response == {}:
             print("No result")
         else:
-            print("[()] {}".format(r.get("id"), r.get("name")))
+            print("[{}] {}".format(response.get("id"), response.get("name")))
     except ValueError:
         print("Not a valid JSON")
