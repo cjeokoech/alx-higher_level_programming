6a78232f41cb36e409c15b9e72faa680acbc459e
cjeokoech <cjeokoech@gmail.com>
2024-04-24 10:56:38 -0700

    úpdate

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 
 const request = require('request');
-const starWar = 'https://swapi-api.hbtn.io/api/films/'.concat(process.argv[2]);
+const starWarsUri = 'https://swapi-api.hbtn.io/api/films/'.concat(process.argv[2]);
 
-request(starwar, function (error, req, body) {
+request(starWarsUri, function (_err, _res, body) {
   const characters = JSON.parse(body).characters;
 
-  for (let i = 0; i < characters.length; ++1) {
-    request(characters[i], function (charError, charReq, charBody) {
-      console.log(JSON.parse(charBody).name;
+  for (let i = 0; i < characters.length; ++i) {
+    request(characters[i], function (_cErr, _cRes, cBody) {
+      console.log(JSON.parse(cBody).name);
     });
   }
 });
